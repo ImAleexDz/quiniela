ec383c93979064381bd1cefe45ce5f07576fa398
Alejandro Diaz Alvarado <alexndro7145@gmail.com>
2025-11-04 20:06:09 -0600

    create teams and matches

---
+++ src/utils/resources.tsx
@@ -0,0 +1,112 @@
+export const soccerTeams = [
+    {
+      id: 1,
+      name: "Cruz Azul",
+      logo: ""
+    },
+    {
+      id: 2,
+      name: "Toluca",
+      logo: ""
+    },
+    {
+      id: 3,
+      name: "América",
+      logo: ""
+    },
+    {
+      id: 4,
+      name: "Tigres",
+      logo: ""
+    },
+    {
+      id: 5,
+      name: "Monterrey",
+      logo: ""
+    },
+    {
+      id: 6,
+      name: "Guadalajara",
+      logo: ""
+    },
+    {
+      id: 7,
+      name: "Juárez",
+      logo: ""
+    },
+    {
+      id: 8,
+      name: "Pachuca",
+      logo: ""
+    },
+    {
+      id: 9,
+      name: "Tijuana",
+      logo: ""
+    },
+    {
+      id: 10,
+      name: "Pumas",
+      logo: ""
+    },
+    {
+      id: 11,
+      name: "Santos",
+      logo: ""
+    },
+    {
+      id: 12,
+      name: "Atlas",
+      logo: ""
+    },
+    {
+      id: 13,
+      name: "Querétaro",
+      logo: ""
+    },
+    {
+      id: 14,
+      name: "Atl. San Luis",
+      logo: ""
+    },
+    {
+      id: 15,
+      name: "Necaxa",
+      logo: ""
+    },
+    {
+      id: 16,
+      name: "Mazatlán",
+      logo: ""
+    },
+    {
+      id: 17,
+      name: "León",
+      logo: ""
+    },
+    {
+      id: 18,
+      name: "Puebla",
+      logo: ""
+    }
+  ]
+
+export const jornadas = [
+  {
+    id: 17,
+    name: "Jornada 17",
+    date_start: "2025-11-07",
+    date_end: "2025-11-09",
+    matches: [
+      { match_id: 1, home_team_id: 7, away_team_id: 13 },
+      { match_id: 2, home_team_id: 16, away_team_id: 15 },
+      { match_id: 3, home_team_id: 9, away_team_id: 12 },
+      { match_id: 4, home_team_id: 4, away_team_id: 14 },
+      { match_id: 5, home_team_id: 6, away_team_id: 5 },
+      { match_id: 6, home_team_id: 17, away_team_id: 18 },
+      { match_id: 7, home_team_id: 2, away_team_id: 3 },
+      { match_id: 8, home_team_id: 1, away_team_id: 10 },
+      { match_id: 9, home_team_id: 11, away_team_id: 8 },
+    ]
+  }
+]
